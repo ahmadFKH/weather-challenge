9f897b2039a6cf79d1188d50db78f020c3874254
Ahmad Khalilieh <ahmadfk90@gmail.com>
2018-05-04 12:02:01 +0300

    deleting posts

--- public/main.js
+++ public/main.js
@@ -1,7 +1,6 @@
 var WeatherApp = function () {
     var STORAGE_ID = 'weather';
     var weathers = [];
-    var weather_id = 0;
     var saveToLocalStorage = function () {
         localStorage.setItem(STORAGE_ID, JSON.stringify(weathers));
     }
@@ -10,7 +9,7 @@ var WeatherApp = function () {
     }
     class Weather {
         constructor(city, temp_c, temp_f, localTime) {
-            this.id = weather_id;
+            this.id = weathers.length;
             this.name = city;
             this.temp_c = temp_c;
             this.temp_f = temp_f;
@@ -31,7 +30,6 @@ var WeatherApp = function () {
                 var jsonObj = JSON.stringify(data);
                 var obj = JSON.parse(jsonObj);
                 var newWeather = new Weather(obj.location.name, obj.current.temp_c, obj.current.temp_f, obj.location.localtime);
-                weather_id++;
                 weathers.push(newWeather);
                 saveToLocalStorage();
                 renderWeather();
@@ -56,35 +54,30 @@ var WeatherApp = function () {
         weathers[id].comments.push(newComment);
         saveToLocalStorage();
     }
-    /*var renderComments = function (id) {
-        weathers = getFromLocalStorage();
-        $('.comments-container').empty();
-        if (weathers.length > 0) {
-            for ( i = 0; i < weathers[i].comments.length; i++) {
-                var source = $('#comment-result-temp').html();
-                var template = Handlebars.compile(source);
-                var newHTML = template(weathers[id].comments[i]);
-                $('.comments-container').append(newHTML);
-            }   
+    var deleteWeather = function(wID) {
+        weathers.splice(wID, 1);
+        for (var i=wID; i<weathers.length; i++) {
+            weathers[wID].id = wID;
         }
-    }*/
+        saveToLocalStorage();
+    }
     renderWeather();
-    //renderComments();
     return {
         getWeather: getWeather,
         renderWeather: renderWeather,
         addComment: addComment,
-        //renderComments: renderComments
+        deleteWeather: deleteWeather
     };
 }
 var app = WeatherApp();
 
-$('.add-city').on('click', function () {
+$('.add-city').on('click keypress', function () {
     var $city = $('#post-city').val();
     app.getWeather($city);
+    $('#post-city').val("");
 });
-$(document).keypress(function (e) {
-    if (e.which == 13) {
+$(document).keypress(function(e) {
+    if(e.which == 13) {
         var $city = $('#post-city').val();
         app.getWeather($city);
     }
@@ -93,5 +86,10 @@ $('.results').on('click', 'button.add-comment',function () {
     var $comment = $(this).closest('ul.result').find('.comment-input').val();
     var wID = $(this).closest('ul.result').data().id;
     app.addComment($comment, wID);
+    app.renderWeather();
+});
+$('.results').on('click', '#delete',function () {
+    var wID = $(this).closest('ul.result').data().id;
+    app.deleteWeather(wID);
     app.renderWeather();
 });
